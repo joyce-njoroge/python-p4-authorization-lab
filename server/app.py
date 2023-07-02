27c55c08e267c9fe5b11227cd62e6d53fe89e0f2
Joyce Njoroge <joycewambuku33@gmail.com>
2023-07-02 10:14:55 +0300

    complete

--- server/app.py
+++ server/app.py
@@ -87,12 +87,24 @@ class CheckSession(Resource):
 class MemberOnlyIndex(Resource):
     
     def get(self):
-        pass
-
+        if not session.get('user_id'):
+            return {'message': 'Unauthorized'}, 401
+        else:
+            articles = [article.to_dict() for article in Article.query.filter_by(is_member_only=True).all()]
+            return make_response(jsonify(articles), 200)
+        
 class MemberOnlyArticle(Resource):
     
     def get(self, id):
-        pass
+        if not session.get('user_id'):
+            return {'message': 'Unauthorized'}, 401
+        else:
+            article = Article.query.filter(Article.id == id).first()
+            if article:
+                article_json = article.to_dict()
+                return article_json, 200
+            else:
+                return {'message': 'Article not found'}, 404
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(IndexArticle, '/articles', endpoint='article_list')
